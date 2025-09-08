c483419075a060102fc79b2a3447924eb56cf387
Snaymi <sneymeb@hotmail.com>
2025-09-08 09:58:13 -0300

    up

--- JavaScript/btn.js
+++ JavaScript/btn.js
@@ -2,3 +2,8 @@ document.addEventListener('exibirTabelaFiltrada', function() {
   const botao = document.getElementById('btn-filtrar-ncm');
   botao.style.display = 'block';
 });
+
+const dropdown = document.getElementById('dropdown');
+dropdown.addEventListener('click', () => {
+  dropdown.classList.toggle('open');
+})
